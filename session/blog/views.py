0ea31f6330ebee01e6a7df8c318f36f39ae9410b
kwonsoyoon <gonsoyun2345@gmail.com>
2024-03-27 23:17:09 +0900

    세션 과제 제출2

---
+++ session/blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Blog
+from django.http import HttpResponse
+
+def home(request):
+    blog = Blog.objects.all()
+    return HttpResponse(blog)
+
+# Create your views here.
